6d050e2bf4114d0167974caf1f375f0fdac0edc1
tenguri-jeon <110875363+tenguri-jeon@users.noreply.github.com>
2022-12-06 12:50:02 +0900

    1313

--- js/main-slide.js
+++ js/main-slide.js
@@ -1,6 +1,11 @@
 const header = document.querySelector('header');
 const headerNav = document.querySelector('nav');
 const innerli = document.querySelector('inner-nav');
+const slideWrapper = document.querySelector('.contents-wrwapper');
+const content = document.getElementsByClassName('content');
+const slideCount = slideWrapper.childElementCount;
+const slideWith = document.querySelector('#main-slide-area').clientWidth;
+let contentWidth = slideWith - 
 
 
 header.addEventListener('mouseover', ()=>{
@@ -11,3 +16,8 @@ header.addEventListener('mouseover', ()=>{
 headerNav.addEventListener('mouseleave', ()=>{
     header.classList.remove('active');
 })
+
+
+function slide() {
+    slideWrapper.style.transform = translateX(`-${contentWidth}`)
+}
